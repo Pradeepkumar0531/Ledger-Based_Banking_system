45927f5bf265f3d57a0c47a3a6071d501ae89f1a
Pradeepkumar0531 <pradeepkumar000531@gmail.com>
2026-03-08 16:02:20 +0530

    Our Backend Ledger is Completed

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ const authRouter = require("./routes/auth.routes");
 const accountRouter = require("./routes/account.routes");
 const transactionRouter = require("./routes/transaction.routes");
 
+app.get('/',(req, res)=>{
+    res.send("Ledger is up and running")
+})
+
 app.use("/api/auth", authRouter);
 app.use("/api/accounts", accountRouter);
 app.use("/api/transactions",transactionRouter)
